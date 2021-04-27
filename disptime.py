77a4bfc9ff0a0e3aebb7852f198b91f9cbf9c48e
Suyash Kelvin Savant <62336754+suyashkelvin@users.noreply.github.com>
2021-04-27 20:17:21 +0530

    changed Time to 16:00

--- disptime.py
+++
@@ -1,9 +0,0 @@
-import time
-from datetime import datetime
-
-
-while True:
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    print("Current Time =", current_time)
-    time.sleep(10)
